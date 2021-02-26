79cc206c77b16b5cc1381c2641c4ef30e68061c6
Daniil Katantsev <liinadk@gmail.com>
2021-02-26 06:11:11 +0500

    create burger +  fix gallery breakpoints + fix partners swiper breakpoints

--- script/swiper-init.js
+++ script/swiper-init.js
@@ -30,21 +30,49 @@ const swiper = new Swiper('.gallery__slider', {
       slidesPerGroup: 2,
     }, 
     1366: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      slidesPerGroup: 2,
+    }, 
+    1920: {
       slidesPerView: 3,
       slidesPerColumn: 2,
       slidesPerGroup: 3,
-    }, 
+    }
   }
 });
 
-const swiper2 = new Swiper('.partners-swiper-container', {
+const swiper2 = new Swiper('.partners__slider', {
   // Optional parameters
+  slidesPerView: 3,
   direction: 'horizontal',
   loop: true,
+  spaceBetween: 0,
+  centerSlides: true,
   
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+    1920: {
+      slidesPerView: 3,
+    }
   }
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.burger').addEventListener('click', function() {
+    document.querySelector('.burger__line--1').classList.toggle('line-active-1')
+    document.querySelector('.burger__line--2').classList.toggle('line-active-2')
+    document.querySelector('.burger__line--3').classList.toggle('line-active-3')
+    document.querySelector('#menu').classList.toggle('is-active')
+  })
 });
