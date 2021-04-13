0f75477d34bd2a16b821a79408d755eb6644066b
Daniil Katantsev <liinadk@gmail.com>
2021-04-14 01:25:20 +0500

    removed useless strings in choices settings

--- script/choices-settings.js
+++ script/choices-settings.js
@@ -1,23 +1,3 @@
-const customDefault = () => {
-    const element = document.querySelector('.styles__select');
-    const choices = new Choices(element, {
-        searchEnabled: false,
-    });
-}
-
-customDefault();
-
-const customSelects = () => {
-    const elements = document.querySelectorAll('.styles__multiselect');
-    elements.forEach(el => {
-    const choices = new Choices(el, {
-        searchEnabled: false, 
-    })
-    });
-}
-
-customSelects();
-
 const customGallerySelect = () => {
     const element = document.querySelector('.filter-form__select');
     const choices = new Choices(element, {
