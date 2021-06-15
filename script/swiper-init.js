e4d5475788813b4f44e061bc23ff989c0d8c4333
Daniil Katantsev <liinadk@gmail.com>
2021-06-15 22:00:36 +0500

    added small fixes for gallery slider on 460

--- script/swiper-init.js
+++ script/swiper-init.js
@@ -8,7 +8,7 @@ function gallerySlider() {
     observer: true,
     observeParents: true,
     observeSlideChildren: true,
-    spaceBetween: 50,
+    spaceBetween: 80,
     
     pagination: {
       el: '.gallery__pagination',
